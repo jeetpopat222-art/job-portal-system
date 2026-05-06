33b1e6c6b824c4050ee95ca68cc8269b628ebcc2
jeetpopat222-art <jeetpopat222@gmail.com>
2026-05-06 22:41:37 +0530

    Implemented user authentication system

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
-from flask import Flask
+from flask import Flask,redirect,render_template,url_for,request,session
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy import Integer, String, ForeignKey,URL
 from sqlalchemy.orm import Mapped, mapped_column,relationship
 from datetime import datetime
-
-
-
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:1234@127.0.0.1:5432/Job_Portal_System"
 db = SQLAlchemy(app)
+app.secret_key = 'your-very-secret-key'
+
+
 
 class User(db.Model):
+
+    __tablename__ = "users"
+
     id:Mapped[int] = mapped_column(primary_key=True)
     username:Mapped[str] = mapped_column(unique=True)
     email:Mapped[str] = mapped_column(unique=True)
@@ -21,22 +25,47 @@ class User(db.Model):
     role:Mapped[str] = mapped_column()
     created_at: Mapped[datetime] = mapped_column(default=datetime.utcnow)
 
+    seeker_profile = relationship("Job_Seeker_Profiles", back_populates="user")
+
+    employer_profile = relationship("Employer_Profiles", back_populates="user")
+
+
+
 class Job_Seeker_Profiles(db.Model):
+
+    __tablename__ = "job_seeker_profiles"
+
     id:Mapped[int] = mapped_column(primary_key=True)
-    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
     full_name:Mapped[str] = mapped_column()
     skills: Mapped[str] = mapped_column()
     education: Mapped[str] =mapped_column()
     resume: Mapped[str] = mapped_column()
 
+    user = relationship("User", back_populates="seeker_profile")
+
+    applications = relationship("Applications", back_populates="job_seeker")
+
+
 class Employer_Profiles(db.Model):
+
+    __tablename__ = "employer_profiles"
+
     id: Mapped[int] = mapped_column(primary_key=True)
-    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
     company_name: Mapped[str] = mapped_column()
     company_description:Mapped[str] = mapped_column()
     website:Mapped[str] = mapped_column()
 
+    user = relationship("User", back_populates="employer_profile")
+
+    jobs = relationship("Job_Posting", back_populates="employer")
+
+
 class Job_Posting(db.Model):
+
+    __tablename__ = "job_postings"
+
     id: Mapped[int] = mapped_column(primary_key=True)
     employer_id: Mapped[int] = mapped_column(ForeignKey("employer_profiles.id"))
     title: Mapped[str] = mapped_column()
@@ -45,17 +74,61 @@ class Job_Posting(db.Model):
     location: Mapped[str] = mapped_column()
     created_at: Mapped[datetime] = mapped_column()
 
+    employer = relationship("Employer_Profiles", back_populates="jobs")
+
+    applications = relationship("Applications", back_populates="job")
+
+
 class Applications(db.Model):
+
+    __tablename__ = "applications"
+    
     id: Mapped[int] = mapped_column(primary_key=True)
-    job_id: Mapped[int] = mapped_column(ForeignKey("job_posting.id"))
+    job_id: Mapped[int] = mapped_column(ForeignKey("job_postings.id"))
     job_seeker_id: Mapped[int] = mapped_column(ForeignKey("job_seeker_profiles.id"))
     status: Mapped[str] = mapped_column()
-    applied_at: Mapped[str] = mapped_column(default=datetime.utcnow)
+    applied_at: Mapped[datetime] = mapped_column(default=datetime.utcnow)
     
 
-@app.route("/")
+    job = relationship("Job_Posting", back_populates="applications")
+
+    job_seeker = relationship(
+        "Job_Seeker_Profiles",
+        back_populates="applications"
+    )
+
+@app.route("/",methods=["GET","POST"])
 def home():
-    return "COMING SOON"
+    return render_template("index.html")
+
+@app.route("/signup",methods=["POST"])
+def signup():
+    username = request.form.get("username")
+    email = request.form.get("email")
+    password = request.form.get("password")
+    role = request.form.get("role")
+
+    hash_password = generate_password_hash(password)
+
+    db.session.add(User(email=email,username=username,password_hash=hash_password,role=role))
+    db.session.commit()
+    
+    return redirect(url_for("home"))
+
+@app.route("/login",methods=["POST"])
+def login():
+    email = request.form.get("email")
+    password = request.form.get("password")
+    user = User.query.filter_by(email=email).first()
+
+    if user and check_password_hash(user.password_hash,password):
+        session["user_id"] = user.id
+        if user.role == "job_seeker":
+            return "WELCOME<YOU WILL FIND JOB HERE"
+        else: 
+            return "YOU WILL FIND YOUR EMPLOYEE HERE>>>>"
+    else:
+        return "Invalid EMAIL or PASSWORD"
 
 if __name__ == "__main__":
     with app.app_context():
